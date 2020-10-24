f5969d2bde019558c2b4519b7d6853b892a35ade
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-25 04:56:33 +0700

    #11 - Add Address Entity

---
+++ src/main/java/com/hendisantika/billingsystem/entity/Address.java
@@ -0,0 +1,54 @@
+package com.hendisantika.billingsystem.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/10/20
+ * Time: 04.55
+ */
+@Data
+@Entity
+@Table(name = "ADDRESS")
+public class Address extends BaseObject {
+
+    @Column(name = "ADDRESS1", nullable = false)
+    private String address1;
+
+    @Column(name = "ADDRESS2", nullable = true)
+    private String address2;
+
+    @Column(name = "CITY", nullable = false)
+    private String city;
+
+    @Column(name = "STATE", nullable = false)
+    private String state;
+
+    @Column(name = "COUNTRY", nullable = false)
+    private String country;
+
+    @Column(name = "LANDMARK", nullable = true)
+    private String landmark;
+
+    @Column(name = "MOBILE", nullable = false)
+    private String mobile;
+
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+}
