7c8032d3d452ab917ae5eb23b54cba3c231691b7
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-26 08:25:58 +0700

    #23 - Add Employee Entity

---
+++ src/main/java/com/hendisantika/billingsystem/entity/Employee.java
@@ -0,0 +1,36 @@
+package com.hendisantika.billingsystem.entity;
+
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/10/20
+ * Time: 08.25
+ */
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee extends BaseObject {
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "employee_code", nullable = false)
+    private String employeeCode;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
