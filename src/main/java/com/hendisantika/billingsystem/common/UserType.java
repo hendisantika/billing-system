e61ae219f7d05991e22d601010a831c2e62915eb
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-26 08:15:49 +0700

    #13 - Add UserType Entity

---
+++ src/main/java/com/hendisantika/billingsystem/common/UserType.java
@@ -0,0 +1,16 @@
+package com.hendisantika.billingsystem.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/10/20
+ * Time: 08.15
+ */
+public enum UserType {
+    EMPLOYEE,
+    CUSTOMER,
+    VENDOR
+}
