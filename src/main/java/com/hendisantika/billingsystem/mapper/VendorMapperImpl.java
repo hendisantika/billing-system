c5ca8e62867006f931bb97b0ec99ef7273004927
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-29 05:03:25 +0700

    #94 - Add VendorMapperImpl

---
+++ src/main/java/com/hendisantika/billingsystem/mapper/VendorMapperImpl.java
@@ -0,0 +1,20 @@
+package com.hendisantika.billingsystem.mapper;
+
+import javax.annotation.processing.Generated;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/20
+ * Time: 05.03
+ */
+@Generated(
+        value = "org.mapstruct.ap.MappingProcessor",
+        date = "2020-10-18T15:59:59+0530",
+        comments = "version: 1.4.1.Final, compiler: javac, environment: Java 1.8.0_261 (Oracle Corporation)"
+)
+public class VendorMapperImpl implements VendorMapper {
+}
