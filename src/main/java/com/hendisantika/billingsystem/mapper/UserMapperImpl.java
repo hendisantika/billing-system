786b1307f1c54c88b4e38203fb8b245f93ffc651
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-29 04:58:09 +0700

    #83 - Add UserMapperImpl

---
+++ src/main/java/com/hendisantika/billingsystem/mapper/UserMapperImpl.java
@@ -0,0 +1,20 @@
+package com.hendisantika.billingsystem.mapper;
+
+import javax.annotation.processing.Generated;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/20
+ * Time: 04.57
+ */
+@Generated(
+        value = "org.mapstruct.ap.MappingProcessor",
+        date = "2020-10-16T17:21:00+0530",
+        comments = "version: 1.4.1.Final, compiler: javac, environment: Java 1.8.0_261 (Oracle Corporation)"
+)
+public class UserMapperImpl implements UserMapper {
+}
