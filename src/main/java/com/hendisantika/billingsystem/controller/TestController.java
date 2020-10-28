2e77f98defb4c2fdc80f6354f379f6e061ad003b
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-29 05:05:40 +0700

    #98 - Add logTest API in TestController

--- src/main/java/com/hendisantika/billingsystem/controller/TestController.java
+++ src/main/java/com/hendisantika/billingsystem/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.hendisantika.billingsystem.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/public/test")
 public class TestController {
+    @GetMapping("/log")
+    public ResponseEntity<Void> logTest() {
+        log.info("This is test log1");
+        log.info("This is test log2");
+        log.info("This is test log3");
+
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
 }
