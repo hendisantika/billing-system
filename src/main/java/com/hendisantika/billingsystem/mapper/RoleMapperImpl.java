d6498e1391c8f91b065470d7c66c9857ee5d5e52
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-29 04:56:46 +0700

    #80 - Add RoleMapperImpl

---
+++ src/main/java/com/hendisantika/billingsystem/mapper/RoleMapperImpl.java
@@ -0,0 +1,21 @@
+package com.hendisantika.billingsystem.mapper;
+
+import javax.annotation.processing.Generated;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/20
+ * Time: 04.56
+ */
+@Generated(
+        value = "org.mapstruct.ap.MappingProcessor",
+        date = "2020-10-19T15:17:09+0530",
+        comments = "version: 1.4.1.Final, compiler: javac, environment: Java 1.8.0_261 (Oracle Corporation)"
+)
+public class RoleMapperImpl implements RoleMapper {
+
+}
