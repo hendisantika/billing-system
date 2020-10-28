bb079ae1063afef787f93cc4b70757a29c3ba5c1
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-29 05:07:24 +0700

    #101 - Add updateEmployee API in UserPublicController

--- src/main/java/com/hendisantika/billingsystem/controller/UserPublicController.java
+++ src/main/java/com/hendisantika/billingsystem/controller/UserPublicController.java
@@ -1,11 +1,20 @@
 package com.hendisantika.billingsystem.controller;
 
+import com.hendisantika.billingsystem.common.UserType;
+import com.hendisantika.billingsystem.dto.UserDTO;
+import com.hendisantika.billingsystem.messages.ResponseMessage;
 import com.hendisantika.billingsystem.service.RegistrationService;
 import com.hendisantika.billingsystem.validator.CustomerValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +34,11 @@ public class UserPublicController {
     CustomerValidator customerValidator;
     @Autowired
     private RegistrationService registrationService;
+
+    @PostMapping("/employees") // http://localhost:9090/api/public/users/employees
+    public ResponseEntity<ResponseMessage<?>> updateEmployee(@Valid @RequestBody UserDTO requestBody) throws Exception {
+        requestBody.setType(UserType.EMPLOYEE.name());
+        ResponseMessage responseMessage = registrationService.doRegistration(requestBody);
+        return new ResponseEntity<ResponseMessage<?>>(responseMessage, HttpStatus.CREATED);
+    }
 }
