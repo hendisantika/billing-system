f6cf7e36cfec7fd8f8539338fae169d1f101609d
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-29 05:05:19 +0700

    #97 - Add TestController

---
+++ src/main/java/com/hendisantika/billingsystem/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.hendisantika.billingsystem.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : billing-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/10/20
+ * Time: 05.05
+ */
+
+@Slf4j
+@RestController
+@RequestMapping("api/public/test")
+public class TestController {
+}
